8ade2b481778e37d41d5a3f825c1d5b2af65326e
15110108@student.hcmute.edu.vn <15110108@student.hcmute.edu.vn>
2023-06-10 10:30:04 +0700

    anyway, commit to say hi the Statuday

--- pages/login.jsx
+++ pages/login.jsx
@@ -14,6 +14,7 @@ const Login = () => {
 
     const handleSubmit = async (event) => {
         //Prevent page reload
+        //slapping the keycap to make something good happen
         event.preventDefault();
 
         var {name, uname, pass} = document.forms[0];
